eb22fd6729328889a3bc261cbe260dd04d34400c
brandonktran <brandontran@berkeley.edu>
2020-07-17 19:55:09 +0000

    user can change views

---
+++ src/components/view-cards.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class ViewCards extends React.Component {
+
+
+  render() {
+    return (
+      <h1 className="text-center">My Cards</h1>
+    );
+  }
+}
+export default ViewCards;
